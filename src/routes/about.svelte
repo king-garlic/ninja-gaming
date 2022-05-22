8188371dc35357ce4b04426db49dbbbc93d97880
sangbinlee <sangbinlee9@gmail.com>
2022-05-22 14:53:37 +0900

    second commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,15 @@
+<script>
+</script>
+
+<div class="about">
+  <h2>about us</h2>
+  <p>asdfasdfasdf asdfasdfasdfsdf</p>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Title from "../lib/title.svelte";
+  import Index from "./index.svelte";
+  let title = "Ninja Gaming Guides";
+  const updateTitle = () => {
+    title = "something completely different";
+  };
+</script>
+
+<div class="index">
+  <!-- <h1>{title}</h1> -->
+  <p>
+    Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+  </p>
+
+  <button on:click={updateTitle}>change title</button>
+  <input type="text" bind:value={title} />
+
+  <a href="/about">about</a>
+  <a href="/guides">guides</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
